adf0a983ad2346b895d35dd0d3072004cca5eeec
Robert Otieno <robertotieno.or@gmail.com>
2023-07-11 04:04:48 +0300

    7112023

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,53 +1,26 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { getCountriesData } from '../api/malariaAPI';
 import * as tf from '@tensorflow/tfjs';
-// const tfjs_model = require('../tfjs_model/model.json')
+
 const StateContext = createContext()
 
-// const initialState = {
-//     chat: false,
-//     cart: false,
-//     userProfile: false,
-//     notification: false,
-// }
-
 export const ContextProvider = ({ children }) => {
-    // const [activeMenu, setActiveMenu] = useState(true)
-    // const [isClicked, setIsClicked] = useState(initialState)
-    // const [screenSize, setScreenSize] = useState(undefined)
     const [countriesData, setCountriesData] = useState([]);
     const [countryId, setCountryId] = useState('');
-    // get weather data from rapidapi
-    // const [location, setLocation] = useState('');
-    // const [currentColor, setCurrentColor] = useState('#03C9D7')
-    // const [currentMode, setCurrentMode] = useState('Light')
-    // const [themeSettings, setThemeSettings] = useState(false)
+    const [location, setLocation] = useState('');
 
-    // const setMode = (e) => {
-    //     setCurrentMode(e.target.value);
-    //     localStorage.setItem('themeMode', e.target.value);
-    // }
-
-    // const setColor = (color) => {
-    //     setCurrentColor(color);
-    //     localStorage.setItem('colorMode', color);
-    // }
-
-    // const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true })
-
-    // Define a functional component to handle the model training and prediction
-
-    // const [model, setModel] = useState(null);
-    // const [newData, setNewData] = useState([
-    //     {
-    //         "year": 2016,
-    //         "precipitation": 59.60018333,
-    //         "relative_humidity": 0.755792502,
-    //         "temperature": 25.75660833,
-    //         // "malaria_incidence": 65.96
-    //     }
-    // ]);
-    // const [predictions, setPredictions] = useState([]);
+    const [model, setModel] = useState(null);
+    const [newData, setNewData] = useState([
+        {
+            "year": 2016,
+            "precipitation": 59.60018333,
+            "relative_humidity": 0.755792502,
+            "temperature": 25.75660833,
+            // "malaria_incidence": 65.96
+        }
+    ]);
+    const [predictions, setPredictions] = useState([]);
+    const MODEL_URL = 'model/model.json';
 
     /**
      * When there is a malaria outbreak, a certain number of cases in a specific location within a certain timeframe must be surpassed,
@@ -69,80 +42,55 @@ export const ContextProvider = ({ children }) => {
     }, []);
 
     // load customised randomforest model
-    // useEffect(() => {
-    //     // Function to load the model
-    //     const loadModel = async () => {
-    //         // load the Random Forest model
-    //         // const model = await tf.loadLayersModel('../../tfjs_model/model.json');
-    //         const model = await tf.loadLayersModel('model.json');
+    useEffect(() => {
+        // Function to load the model
+        const loadModel = async () => {
+            // load the Random Forest model
+            const model = await tf.loadGraphModel(MODEL_URL);
 
-    //         // Set the loaded model
-    //         setModel(model);
-    //     };
+            // Set the loaded model
+            console.log('Model loaded!')
+            setModel(model);
+        };
 
-    //     loadModel();
-    // }, []);
+        loadModel();
+    }, []);
 
-    // useEffect(() => {
-    //     // Function to make predictions
-    //     const predict = () => {
-    //         // Return if model is not trained or there is no new data
-    //         if (!model || newData.length === 0) return;
+    useEffect(() => {
+        // Function to make predictions
+        const predict = () => {
+            // Return if model is not trained or there is no new data
+            if (!model || newData.length === 0) return;
 
-    //         // Extract the features from the new data
-    //         const newFeatures = newData.map((data) => [
-    //             data.precipitation,
-    //             data.relative_humidity,
-    //             data.temperature,
-    //         ]);
-    //         console.log(newFeatures);
+            // Extract the features from the new data
+            // const newFeatures = newData.map((data) => [
+            //     data.precipitation,
+            //     data.relative_humidity,
+            //     data.temperature,
+            // ]);
 
-    //         // Prepare your input data as a Tensor
-    //         const newFeaturesTensor = tf.tensor2d(newFeatures, [newFeatures.length, 3]);
+            // Prepare your input data as a Tensor
+            // const newFeaturesTensor = tf.tensor2d(newFeatures, [newFeatures.length, 3]);
+            const input = tf.tensor2d([[2016, 59.60018333, 0.755792502, 25.75660833]]);
 
-    //         // Make predictions
-    //         const result = model.predict(newFeaturesTensor);
-    //         setPredictions(result);
+            // Make predictions
+            // const result = model.predict(newFeaturesTensor);
+            const prediction = model.predict(input);
+            console.log(prediction.dataSync());
+            // setPredictions(result);
 
-    //         // Clean up
-    //         tf.dispose(newFeaturesTensor);
-    //     };
+            // Clean up
+            // tf.dispose(newFeaturesTensor);
+        };
 
-    //     // Make predictions when newData or model changes
-    //     predict();
-    // }, [newData, model]);
-
-    // useEffect(() => {
-    //     // Function to make predictions
-    //     const predict = async () => {
-    //         // Return if model is not trained or there is no new data
-    //         // if (!model || newData.length === 0) return;
-
-    //         const model = await tf.loadLayersModel('');
-
-    //         // Extract the features from the new data
-    //         const newFeatures = newData.map((data) => [data.precipitation, data.relative_humidity, data.temperature]);
-
-    //         // Convert new features data to a tensor
-    //         const newFeaturesTensor = tf.tensor2d(newFeatures, [newFeatures.length, 3]);
-
-    //         // Make predictions
-    //         const result = model.predict(newFeaturesTensor).dataSync();
-    //         setPredictions(result);
-
-    //         // Clean up
-    //         tf.dispose(newFeaturesTensor);
-    //     };
-
-    //     // Make predictions when newData or model changes
-    //     predict();
-    // }, [newData]);
-
+        // Make predictions when newData or model changes
+        predict();
+    }, [newData, model]);
 
     // console.log(`new prediction: ${predictions}`);
 
     return (
-        <StateContext.Provider value={{ countriesData, countryId, setCountryId }}>
+        <StateContext.Provider value={{ countriesData, countryId, setCountryId, location, setLocation, setNewData }}>
             {children}
         </StateContext.Provider>
     )
